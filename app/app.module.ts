30dc4c9b4889e30599ce55bcd95e35b26abecd9b
austinlangdon <alangdon93@gmail.com>
2016-09-29 18:23:27 -0700

    initial routing implementation

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,19 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
-import {  AppComponent } from './app.component';
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
+import { routing } from './app.routing';
+import { DashboardComponent } from './dashboard.component';
 
 @NgModule({
     imports: [
         BrowserModule,
-        FormsModule
+        FormsModule,
+        routing
     ],
     declarations: [
         AppComponent,
+        DashboardComponent,
+        HeroesComponent,
         HeroDetailComponent
     ],
-    bootstrap: [AppComponent]
+    providers: [
+        HeroService
+    ],
+    bootstrap: [ AppComponent ]
 })
 
 export class AppModule { }
