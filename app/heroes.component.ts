ec46dc24cc5ceaa7293efa0e0047f65b156c39ca
austinlangdon <alangdon93@gmail.com>
2016-09-30 20:17:48 -0700

    adds delete feature to heroes list

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -39,7 +39,14 @@ export class HeroesComponent implements OnInit {
                 this.heroes.push(hero);
                 this.selectedHero = null;
             });
-    } 
+    }
+
+    delete(hero: Hero): void {
+        this.heroService.delete(hero.id).then(() => {
+            this.heroes = this.heroes.filter(h => h !== hero);
+            if (this.selectedHero === hero) { this.selectedHero = null; }
+        });
+    }
 
     gotoDetail(): void {
         let link = ['/detail', this.selectedHero.id];
